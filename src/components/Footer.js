fdbd3987cc7ee8edcdf3df2fa5ebf0f7cbae37a5
Manojass18 <manojassmanojass12@gmail.com>
2023-11-17 14:15:56 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export function Footer(){
+  const name1="manoja";
+  function myfunc(){
+    alert("button is clicked")
+}
+function Footer() {
+  return (
+    <div>
+       <p style={{color:'yellow',fontWeight:'bold'}}>This is Footer</p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import "./Header.css"
+export default function Header() {
+  return (
+    <div><h1>Manoja S S</h1></div>
+  )
+}
